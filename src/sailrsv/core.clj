93746d673628301813a6f147a0cb9db90aa1f88b
Mike Piech <mpiech@alum.mit.edu>
2022-07-08 07:56:49 -0700

    cleaned up working SQL

--- src/sailrsv/core.clj
+++ src/sailrsv/core.clj
@@ -266,12 +266,9 @@
       (jdbc/delete! dbspec :reservations
                     ["DATE(res_date)=?" res-datestr])
       (jdbc/delete! dbspec :reservations
-                    ["date_trunc('day',res_date)= CAST (? AS TIMESTAMP)"
+                    [(str "date_trunc('day',res_date)="
+                          "CAST (? AS TIMESTAMP)")
                      res-datestr])
-;      (jdbc/execute! dbspec
-;                     [(str
-;                       "DELETE FROM reservations WHERE res_date ="
-;                       " CAST ('" res-datestr "' AS TIMESTAMP)")])
       )
     (jdbc/insert! dbspec :cancellations
                   {:cancel_date check-dtstr
